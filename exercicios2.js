930e98962b7390e315516558c22d67d3b2843dcd
CVMScris <102757901+CVMScris@users.noreply.github.com>
2022-05-05 15:40:02 -0300

    Add files via upload

---
+++ exercicios2.js
@@ -0,0 +1,41 @@
+//1-
+
+function nome (a){
+    console.log(a)
+    return
+}
+
+nome ("Cristiane")
+
+//2-
+
+function soma (f,g){
+    console.log(f+g)
+    return
+}
+
+soma(9,7)
+
+//3-
+
+function mult (k,l){
+    console.log(k*l)
+    return
+}
+
+soma(19,6)
+
+//4-
+
+function conta (s,o,u,v){
+    let z = s+o+u+v;
+    console.log (z/4)
+    return
+}
+
+conta(10,8,9,10)
+
+
+
+
+
